98d7552b67a9a2732b1785f08d10595b5cf93935
DevTiti <petrit.kabashi10@gmail.com>
2026-08-25 23:02:28 +0200

    completed the project

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 import traceback
 import uvicorn
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+
 from backend import run_travel_agent
+
+# This is to allow nested event loops for async calls in FastAPI
+import nest_asyncio
+nest_asyncio.apply()
+
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -27,6 +34,7 @@ app.mount(
 templates = Jinja2Templates(
     directory=str(BASE_DIR / "templates")
 )
+
 
 
 class TravelRequest(BaseModel):
--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-from tools.tavily_tool import tavily_search
-from tools.flight_tool import search_flights
-from backend import run_travel_agent
+# from tools.tavily_tool import tavily_search
+# from tools.flight_tool import search_flights
+# from backend import run_travel_agent
 
-# res = tavily_search("Best hotels in Turkey")
-# print(res)
-# res = search_flights("Plan a 7 days to Kosovo trip from Turkey")
-# print(res)
+# # res = tavily_search("Best hotels in Turkey")
+# # print(res)
+# # res = search_flights("Plan a 7 days to Kosovo trip from Turkey")
+# # print(res)
 
-user_input = input("Enter travel request: ")
+# user_input = input("Enter travel request: ")
 
-response = run_travel_agent(
-    user_input=user_input,
-    thread_id="test_user"
-)
+# response = run_travel_agent(
+#     user_input=user_input,
+#     thread_id="test_user"
+# )
 
-print("\nFINAL RESPONSE:\n")
-print(response["answer"])
+# print("\nFINAL RESPONSE:\n")
+# print(response["answer"])
+
+import asyncio
+#from mcp_client_test import get_all_tools
+from mcp_client import get_all_tools
+
+if __name__ == "__main__":
+    asyncio.run(get_all_tools())
